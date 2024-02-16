4086ff22f85836cb27a3a9af480bdc34924f6d09
98emre <emre-98@live.se>
2024-02-16 14:33:30 +0100

    feat: added solution for lexicon assignment

---
+++ NoDuplicates/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace NoDuplicates {
+
+
+    class Program {
+
+        static void Main(string[] args){
+            
+            Console.WriteLine("Application Started");
+            //Console.WriteLine(CheckForDuplicates("THE RAIN IN SPAIN"));
+            //Console.WriteLine(CheckForDuplicates("IN THE RAIN AND THE SNOW"));
+
+            string input = Console.ReadLine().ToUpper();
+
+            if(input.Length > 80){
+                Console.WriteLine("Input length can't be more than 80");
+                return;
+            }
+            
+            Console.WriteLine(CheckForDuplicates(input) ? "no" : "yes");
+
+        }
+
+        static bool CheckForDuplicates(string input) {
+            string[] words = input.Split(' ');
+            HashSet<string> seenWords = new HashSet<string>();
+
+            foreach(var word in words) {
+                if(!seenWords.Add(word)) {
+                    return true; 
+                }
+            }
+
+            return false; 
+        }
+    }
+}
